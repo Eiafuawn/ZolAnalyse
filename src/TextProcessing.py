0f35701e71ab39cb929227b08b96ff0beae0e3d8
Kenan Henzelin <kenan.henzelin@proton.me>
2023-11-05 13:35:57 +0100

    Using only spaCy

--- src/TextProcessing.py
+++ src/TextProcessing.py
@@ -1,8 +1,8 @@
 import re
 import string
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 import spacy
+from spacy import displacy
+# nlp = spacy.load('fr_dep_news_trf')
 nlp = spacy.load('fr_core_news_sm')
 
 
@@ -11,48 +11,46 @@ class TextProcesser:
         with open(path, encoding='utf-8') as f:
             self.raw_txt = f.read()
         self.preprocess()
-        self.tokenize(self.processed_txt)
-        self.filter()
-        self.tag()
         
 
 
     def preprocess(self):
-        self.processed_txt = self.raw_txt.lower()
+        # Process the text by removing punctuation, special chars and making it lowercase
         self.processed_txt = self.raw_txt.translate(str.maketrans('', '', string.punctuation))
         self.processed_txt = re.sub(r'[^\w\s]', '', self.raw_txt)
+        self.processed_txt = self.raw_txt.lower()
+        self.doc = nlp(self.processed_txt)
+        self.sentences = list(self.doc.sents)
+        self.entities = [(e.text, e.start_char, e.end_char, e.label_) for e in self.doc.ents]
 
 
-    def tokenize(self, txt):
-        self.tokenized_txt = word_tokenize(txt)
+    def render(self, opt):
+        if (opt == "entities"):
+            displacy.serve(self.doc, style='ent')
 
 
-    def filter(self):
-        self.filtered_tokens = [
-            word for word in self.tokenized_txt
-                if word not in stopwords.words('french')
-        ]
-
-
-    def tag(self):
-        self.filtered_txt = " ".join(self.filtered_tokens)
-        self.tagged = nlp(self.filtered_txt)
-
-
-    def processedToFile(self):
-        with open("processed.txt", "w", encoding='utf-8') as f:
-            f.write(self.filtered_txt)
+    """ def getSentence(self, print):
+        if (print == False):
+            with open("sentences.txt", "w", encoding='utf-8') as f:
+                for s in self.sentences:
+                    f.write(s.text)
+        else:
+            print(self.sentences)
     
-    def full_print(self):
-        print(self.filtered_txt)
-        print(self.tagged)
-        print(self.tagged.ents)
-        print(self.tagged.noun_chunks)
-        for token in self.tagged:
-            print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-                token.shape_, token.is_alpha, token.is_stop)
 
-    def showEntities(self):
-        with open("entities.txt", "w", encoding='utf-8') as f:
-            for ent in self.tagged.ents:
-                f.write(ent.text + " " + ent.label_ + "\n")
+    def getProcessedTxt(self, print):
+        if (print == False):
+            with open("processed.txt", "w", encoding='utf-8') as f:
+                f.write(self.processed_txt)
+        else:
+            print(self.processed_txt)
+
+
+    def getEntities(self, print):
+        if (print == False):
+            with open("entities.txt", "w", encoding='utf-8') as f:
+                for ents in self.entities:
+                    f.write(str(ents) + "\n")
+        else:
+            print(self.entities)
+ """
--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,10 @@ from TextProcessing import TextProcesser
 def main():
     path = "C:\\Users\\kenan\\code\\python\\nlp\\characterAnalysis\\letranger.txt"
     textProcesser = TextProcesser(path)
-    textProcesser.printEntities()
+    textProcesser.render("entities")
+    # textProcesser.getEntities(False)
+    # textProcesser.getSentence(False)
+    # textProcesser.getProcessedTxt(False)
 
 if __name__ == "__main__":
     main()
